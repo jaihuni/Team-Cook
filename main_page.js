975de6949edfa2c0e0f164f4b75780f54af55939
jaihuni <kjhyama@gmail.com>
2020-06-09 02:23:05 +0900

    Fix script bug temporary

--- main_page.js
+++ main_page.js
@@ -1,10 +1,12 @@
 $(document).ready( function() {
     $(document).on('click', '#suggest',function() {
         $("#box").load("suggest.html");
+        $("#box").load("suggest.html");
         $("#left").text("Recipe Suggestion");
     });
 	
     $(document).on('click', '#recipes',function() {
+        $("#box").load("recipes.html");
         $("#box").load("recipes.html");
         $("#left").text("Recipe Optimizing");
     });
