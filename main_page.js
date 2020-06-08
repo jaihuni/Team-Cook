f0aa2c2aa567f9c405575c6213a037057820d67d
jaihuni <kjhyama@gmail.com>
2020-06-09 02:40:17 +0900

    preload resource

--- main_page.js
+++ main_page.js
@@ -1,12 +1,10 @@
 $(document).ready( function() {
     $(document).on('click', '#suggest',function() {
-        $("#box").load("suggest.css");
         $("#box").load("suggest.html");
         $("#left").text("Recipe Suggestion");
     });
 	
     $(document).on('click', '#recipes',function() {
-        $("#box").load("recipes.css");
         $("#box").load("recipes.html");
         $("#left").text("Recipe Optimizing");
     });
